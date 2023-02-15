2abe6bc0de61575dfdeff610049ca0c67c47816a
Teruteru <teruterubouzu1024@gmail.com>
2023-02-16 02:58:55 +0900

    Add translation resources and test

---
+++ src/test/java/com/github/albatross256/PetBall/LoreWriter/VillagerLoreWriterBundleTest.java
@@ -0,0 +1,19 @@
+package com.github.albatross256.PetBall.LoreWriter;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.junit.jupiter.api.Test;
+
+public class VillagerLoreWriterBundleTest {
+
+  private ResourceBundle bundle = ResourceBundle.getBundle("com.github.albatross256.PetBall.LoreWriter.VillagerLoreWriter", Locale.JAPANESE);
+
+  @Test
+  void farmerTest() {
+    assertEquals(bundle.getString("Farmer"), "農民");
+  }
+
+}
