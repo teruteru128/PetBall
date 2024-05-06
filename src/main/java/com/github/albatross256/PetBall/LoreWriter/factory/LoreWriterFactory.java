a1e1ddfebae2a4766282f206277e1dc774b03530
Teruteru <teruterubouzu1024@gmail.com>
2024-05-06 23:48:32 +0900

    LoreWriterFactoryをリファクタリング

--- src/main/java/com/github/albatross256/PetBall/LoreWriter/factory/LoreWriterFactory.java
+++ src/main/java/com/github/albatross256/PetBall/LoreWriter/factory/LoreWriterFactory.java
@@ -6,97 +6,52 @@ import org.bukkit.entity.EntityType;
 public class LoreWriterFactory {
 
 	public LoreWriter newLoreWriter(EntityType entityType) {
-		switch(entityType) {
-		case VILLAGER:
-			return new VillagerLoreWriter();
-		case PARROT:
-			return new ParrotLoreWriter();
-		case CAT:
-			return new CatLoreWriter();
-		case OCELOT:
-			return new OcelotLoreWriter();
-		case WOLF:
-			return new WolfLoreWriter();
-		case COW:
-			return new CowLoreWriter();
-			case MOOSHROOM:
-			return new MushroomLoreWriter();
-		case PIG:
-			return new PigLoreWriter();
-		case SHEEP:
-			return new SheepLoreWriter();
-		case CHICKEN:
-			return new ChickenLoreWriter();
-		case RABBIT:
-			return new RabbitLoreWriter();
-		case POLAR_BEAR:
-			return new PolarBearLoreWriter();
-		case TURTLE:
-			return new TurtleLoreWriter();
-		case DOLPHIN:
-			return new DolphinLoreWriter();
-		case SQUID:
-			return new SquidLoreWriter();
-		case IRON_GOLEM:
-			return new IronGolemLoreWriter();
-			case SNOW_GOLEM:
-			return new SnowmanLoreWriter();
-		case BAT:
-			return new BatLoreWriter();
-		case WANDERING_TRADER:
-			return new WanderingTraderLoreWriter();
-		case PANDA:
-			return new PandaLoreWriter();
-		case FOX:
-			return new FoxLoreWriter();
-		case LLAMA:
-			return new LlamaLoreWriter();
-		case MULE:
-			return new MuleLoreWriter();
-		case DONKEY:
-			return new DonkeyLoreWriter();
-		case COD:
-			return new CodLoreWriter();
-		case SALMON:
-			return new SalmonLoreWriter();
-		case TROPICAL_FISH:
-			return new TropicalFishLoreWriter();
-		case PUFFERFISH:
-			return new PufferFishLoreWriter();
-		case BEE:
-			return new BeeLoreWriter();
-		case HORSE:
-			return new HorseLoreWriter();
-		case ZOMBIE_HORSE:
-			return new ZombieHorseLoreWriter();
-		case SKELETON_HORSE:
-			return new SkeletonHorseLoreWriter();
-		case TRADER_LLAMA:
-			return new TraderLlamaLoreWriter();
-		case PIGLIN:
-			return new PiglinLoreWriter();
-		case STRIDER:
-			return new StriderLoreWriter();
-		case AXOLOTL:
-			return new AxolotlLoreWriter();
-		case GOAT:
-			return new GoatLoreWriter();
-		case GLOW_SQUID:
-			return new GlowSquidLoreWriter();
-		case ALLAY:
-			return new AllayLoreWriter();
-		case FROG:
-			return new FrogLoreWriter();
-		case TADPOLE:
-			return new TadpoleLoreWriter();
-		case CAMEL:
-			return new CamelLoreWriter();
-		case SNIFFER:
-			return  new SnifferLoreWriter();
-		case ARMADILLO:
-			return  new ArmadilloLoreWriter();
-		default:
-			return null;
-		}
+        return switch (entityType) {
+            case VILLAGER -> new VillagerLoreWriter();
+            case PARROT -> new ParrotLoreWriter();
+            case CAT -> new CatLoreWriter();
+            case OCELOT -> new OcelotLoreWriter();
+            case WOLF -> new WolfLoreWriter();
+            case COW -> new CowLoreWriter();
+            case MOOSHROOM -> new MushroomLoreWriter();
+            case PIG -> new PigLoreWriter();
+            case SHEEP -> new SheepLoreWriter();
+            case CHICKEN -> new ChickenLoreWriter();
+            case RABBIT -> new RabbitLoreWriter();
+            case POLAR_BEAR -> new PolarBearLoreWriter();
+            case TURTLE -> new TurtleLoreWriter();
+            case DOLPHIN -> new DolphinLoreWriter();
+            case SQUID -> new SquidLoreWriter();
+            case IRON_GOLEM -> new IronGolemLoreWriter();
+            case SNOW_GOLEM -> new SnowmanLoreWriter();
+            case BAT -> new BatLoreWriter();
+            case WANDERING_TRADER -> new WanderingTraderLoreWriter();
+            case PANDA -> new PandaLoreWriter();
+            case FOX -> new FoxLoreWriter();
+            case LLAMA -> new LlamaLoreWriter();
+            case MULE -> new MuleLoreWriter();
+            case DONKEY -> new DonkeyLoreWriter();
+            case COD -> new CodLoreWriter();
+            case SALMON -> new SalmonLoreWriter();
+            case TROPICAL_FISH -> new TropicalFishLoreWriter();
+            case PUFFERFISH -> new PufferFishLoreWriter();
+            case BEE -> new BeeLoreWriter();
+            case HORSE -> new HorseLoreWriter();
+            case ZOMBIE_HORSE -> new ZombieHorseLoreWriter();
+            case SKELETON_HORSE -> new SkeletonHorseLoreWriter();
+            case TRADER_LLAMA -> new TraderLlamaLoreWriter();
+            case PIGLIN -> new PiglinLoreWriter();
+            case STRIDER -> new StriderLoreWriter();
+            case AXOLOTL -> new AxolotlLoreWriter();
+            case GOAT -> new GoatLoreWriter();
+            case GLOW_SQUID -> new GlowSquidLoreWriter();
+            case ALLAY -> new AllayLoreWriter();
+            case FROG -> new FrogLoreWriter();
+            case TADPOLE -> new TadpoleLoreWriter();
+            case CAMEL -> new CamelLoreWriter();
+            case SNIFFER -> new SnifferLoreWriter();
+            case ARMADILLO -> new ArmadilloLoreWriter();
+            default -> null;
+        };
 	}
 }
