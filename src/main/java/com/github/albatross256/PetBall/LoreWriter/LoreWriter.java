fbfdaf33bbcddcb671578ee9bfb04103f02379bf
Teruteru <teruterubouzu1024@gmail.com>
2024-05-12 23:13:19 +0900

    LoreWriter.getHealthMeterをリファクタリング

--- src/main/java/com/github/albatross256/PetBall/LoreWriter/LoreWriter.java
+++ src/main/java/com/github/albatross256/PetBall/LoreWriter/LoreWriter.java
@@ -48,21 +48,12 @@ public abstract class LoreWriter {
 	}
 
 	protected String getHealthMeter(double currentHealth, double maxHealth) {
-		double rate = currentHealth * 20  /  maxHealth;
+		int rate = (int) (currentHealth * 20  /  maxHealth);
 
-		String healthMeter = ChatColor.GREEN.toString() + "";
-
-		int i = 0;
-		for(; i < rate; i ++) {
-			healthMeter = healthMeter + ":";
-		}
-		healthMeter = healthMeter + ChatColor.GRAY.toString() + "";
-		for(; i < 20; i++) {
-			healthMeter = healthMeter + ":";
-		}
-		healthMeter = healthMeter + ChatColor.DARK_PURPLE.toString();
-
-		return "体力 : [" + healthMeter + "]  (" + String.valueOf(Math.round(currentHealth)) + " / " + String.valueOf(Math.round(maxHealth)) + ")";
+		return "体力 : [" + ChatColor.GREEN
+				+ ":".repeat(rate) + ChatColor.GRAY + ":".repeat(20 - rate)
+				+ ChatColor.DARK_PURPLE + "]  (" + currentHealth + " / "
+				+ maxHealth + ")";
 	}
 
 
